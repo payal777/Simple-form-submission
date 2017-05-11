cafca5038c52f7cd871b6c9182f6e690fd8233eb
Payal Mulik <payalmulik707@gmail.com>
2017-05-11 18:03:29 +0530

    Add files via upload

---
+++ firstExample.php
@@ -0,0 +1,9 @@
+<html>
+	<body>
+		Your Name:<?php echo $_POST['yourname'];?><br/>
+		Email:<?php echo $_POST['email'];?><br/>
+		Highest Qualification:<?php echo $_POST['qualification'];?><br/>
+        Mobile Number:<?php echo $_POST['mobileno'];?><br/>
+		Address:<?php echo $_POST['address'];?><br/>
+	</body>
+</html>
